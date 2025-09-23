6341f0cdc5f14afc11b7778f84814e7bbbf30c79
elvispaico <epaico.c@gmai.com>
2025-09-23 10:10:29 -0500

    feat: emitir mensaje

--- sockets/socket.js
+++ sockets/socket.js
@@ -8,5 +8,11 @@ io.on('connection', client => {
 
     client.on('message', (payload) => {
         console.log(payload);
-    })
+    });
+
+     client.on('canal_mensaje', (payload) => {
+        console.log('escuchando canal_mensaje');
+        console.log(payload);
+        client.broadcast.emit('nuevo_mensaje', payload);
+    });
 });
