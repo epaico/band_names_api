9f9134bc3179efce4c4a5df453fb4ca991295bef
elvispaico <epaico.c@gmai.com>
2025-09-24 19:08:52 -0500

    feat: crud bandas

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,7 +1,17 @@
 const {io} = require('../index');
+const Bands = require('../models/bands');
+const Band = require('../models/band');
+
+const bands = new Bands();
+bands.addBand(new Band('Grupo 5'));
+bands.addBand(new Band('Agua Marina'));
+bands.addBand(new Band('Los Chapis'));
+bands.addBand(new Band('Hermanos Yaipen'));
 
 io.on('connection', client => {
     console.log('Cliente conectado');
+    client.emit('bandas_activas', bands.bands);    
+
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
     });
@@ -10,9 +20,19 @@ io.on('connection', client => {
         console.log(payload);
     });
 
-     client.on('canal_mensaje', (payload) => {
-        console.log('escuchando canal_mensaje');
-        console.log(payload);
-        client.broadcast.emit('nuevo_mensaje', payload);
+    client.on('votar_banda', (payload) => {
+       bands.voteBand(payload.id);
+       io.emit('bandas_activas', bands.bands);
     });
+
+    client.on('nueva_banda', (payload) => {
+       bands.addBand(new Band(payload.name));
+       io.emit('bandas_activas', bands.bands);
+    });
+
+    client.on('eliminar_banda', (payload) => {
+       bands.deleteBand(payload.id);
+       io.emit('bandas_activas', bands.bands);
+    });
+
 });
